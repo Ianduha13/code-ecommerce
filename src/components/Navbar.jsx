5aff0acbae87d86ca602868f4b0d75f112cdb728
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-22 09:23:45 -0300

    Adding navbar and theme functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./styles/navbar.css"
+import { FaShoppingCart, FaMoon, FaSun } from "react-icons/fa"
+const Navbar = ({ theme, handleTheme }) => {
+	return (
+		<nav className='navbar-container'>
+			<section className='navbar-left'>Codealo E-commerce</section>
+			<section className='navbar-right'>
+				<button className='nav-btn'>
+					<FaShoppingCart className='nav-icon' size={"1.4em"} />
+				</button>
+				<button className='nav-btn' onClick={() => handleTheme()}>
+					{theme === false ? (
+						<FaSun className='nav-icon' size={"1.4em"} />
+					) : (
+						<FaMoon className='nav-icon' size={"1.4em"} />
+					)}
+				</button>
+			</section>
+		</nav>
+	)
+}
+export default Navbar
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+	return (
+		<section className='landing-page'>
+			<header className='header'>asdasdasdasd</header>
+			<section className='carousel'>asdasdasdas</section>
+		</section>
+	)
+}
+export default LandingPage
